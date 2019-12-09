0d74aa30dada879899d9911adb1061f3f36adbed
Vanz Investor <vanzinvestor@gmail.com>
2019-12-09 11:50:33 +0700

    Todo List Component

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -3,11 +3,26 @@ import TodoItem from './TodoItem';
 
 export default class TodoList extends Component {
   render() {
+    const { items, clearList, handleDelete, handleEdit } = this.props;
     return (
-      <section>
-        <h2>Todo List</h2>
-        <TodoItem />
-      </section>
+      <ul className="list-groutp my-5">
+        <h3 className="text-capitalize text-center">todo list</h3>
+        {items.map(item => (
+          <TodoItem
+            key={item.id}
+            title={item.title}
+            HandleDelete={() => handleDelete(item.id)}
+            handleEdit={() => handleEdit(item.id)}
+          />
+        ))}
+        <button
+          type="button"
+          className="btn btn-danger btn-block text-uppercase mt-5"
+          onClick={clearList}
+        >
+          clear list
+        </button>
+      </ul>
     );
   }
 }
